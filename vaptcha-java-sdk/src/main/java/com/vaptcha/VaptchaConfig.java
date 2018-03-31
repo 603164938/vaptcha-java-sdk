9b6bb80ed04de0c559e34cc60a6322832ed7731c
603164938 <37961817+603164938@users.noreply.github.com>
2018-04-01 04:27:26 +0800

    Update VaptchaConfig.java

--- vaptcha-java-sdk/src/main/java/com/vaptcha/VaptchaConfig.java
+++ vaptcha-java-sdk/src/main/java/com/vaptcha/VaptchaConfig.java
@@ -4,11 +4,11 @@ public class VaptchaConfig {
     /// <summary>
     /// 验证单元ID
     /// </summary>
-    public static final String VID = "59f81616a485d5214ce5901e";
+    public static final String VID = "5abfce51a485d904100efffb";
     /// <summary>
     /// 验证单元密钥
     /// </summary>
-    public static final String KEY = "e3ecac73e8dd4acfbb23b4e07e791a94";
+    public static final String KEY = "0b6b56d75b1b415a96e1405862b0a62b";
     /// <summary>
     /// SDK版本号
     /// </summary>
